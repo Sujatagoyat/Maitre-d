705b846417b9267163c717e8a44d8a2d54ef9b62
Sujatagoyat <sujatagoyat8@gmail.com>
2024-03-29 11:09:59 +0530

    nav bar done

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector(".menuIcon")
+const menulist = document.querySelector(".menulist")
+
+
+menuIcon.addEventListener("click", function () {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overFlowHidden")
+    document.querySelector(".menuIcon").classList.toggle("cross")
+})
