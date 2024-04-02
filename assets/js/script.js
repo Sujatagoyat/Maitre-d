05725c33b9a114390cca606562a5d04a84ac49b0
Sujatagoyat <sujatagoyat8@gmail.com>
2024-04-02 11:05:24 +0530

    contact us section static done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,42 @@ menuIcon.addEventListener("click", function () {
     document.body.classList.toggle("overFlowHidden")
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
+// ================== slider =================================
+$('.responsive').slick({
+    dots: false,
+    infinite: false,
+    autoplay:true,
+    speed: 300,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
